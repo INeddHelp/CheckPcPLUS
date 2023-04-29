c2c1ca7cd7d7e71d513a033b9496100f60a22ce8
INeedHelp <leonardo.bartolelli@gmail.com>
2023-04-29 18:29:31 +0200

    v0.3.1

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
 require 'sys/proctable'
 
-procs = Sys::ProcTable.ps()
+procs = Sys::ProcTable.ps
 
-print "Do you want to save the log file? (Y/N): "
+print 'Do you want to save the log file? (Y/N): '
 save_log = gets.chomp.downcase == 'y'
 
 if save_log
+  puts 'This process might take a minute or so. Please wait...'
   log_file_path = File.join(File.expand_path('~/Desktop'), 'processes.log')
   File.open(log_file_path, 'w') do |f|
     procs.each do |p|
@@ -16,11 +18,21 @@ if save_log
       f.write("Port(s) used: #{ports}\n")
 
       f.write("---------------------\n")
-      puts "Done"
     end
   end
 
   puts "Log file saved to #{log_file_path}"
 else
-  puts "Log not saved"
+  puts 'Log not saved'
+end
+
+print 'Do you want to run a ClamAV scan and quarantine any viruses found? (Y/N): '
+run_clamav = gets.chomp.downcase == 'y'
+
+if run_clamav
+  print 'Please enter your password for sudo: '
+  password = gets.chomp
+
+  puts 'Running ClamAV scan. This might take a while. Please wait...'
+  `echo "#{password}" | sudo -S clamscan --move=/home/user/quarantine -r /`
 end
